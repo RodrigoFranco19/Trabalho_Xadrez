718a904671a0dc695cb6ba75761189cc8a28c177
Rodrigo Franco <rodrigopf1905@gmail.com>
2020-01-24 10:14:11 -0300

    Possiveis movimentos do Rei

--- src/Pecas/Rei.java
+++ src/Pecas/Rei.java
@@ -1,8 +1,7 @@
 package Pecas;
 
-import Tabuleiro_Jogo.Tabuleiro;
-import xadrez.Cor;
-import xadrez.Xadrez_Peca;
+import Tabuleiro_Jogo.*;
+import xadrez.*;
 
 public class Rei extends Xadrez_Peca {
 
@@ -15,9 +14,67 @@ public class Rei extends Xadrez_Peca {
 		return "K";
 	}
 
+	private boolean canMove(Posicao pos) {
+		Xadrez_Peca p = (Xadrez_Peca) getTab().piece(pos);
+		
+		return p == null && p.getC() != getC();
+	}
+	
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][]mat = new boolean[getTab().getLinha()][getTab().getColuna()];
+		
+		Posicao p1 = new Posicao(0,0);
+		
+		//Cima
+		p1.SetValues(p1.getLinha() - 1, p1.getColuna());
+		while(getTab().PositionExists(p1) && canMove(p1)) {
+			mat[p1.getLinha()][p1.getColuna()] = true;
+		}
+		
+		//Baixo
+		p1.SetValues(p1.getLinha() + 1, p1.getColuna());
+		while(getTab().PositionExists(p1) && canMove(p1)) {
+			mat[p1.getLinha()][p1.getColuna()] = true;
+		}
+		
+		//Esquerda
+		p1.SetValues(p1.getLinha(), p1.getColuna() -1);
+		while(getTab().PositionExists(p1) && canMove(p1)) {
+			mat[p1.getLinha()][p1.getColuna()] = true;
+		}
+		
+		//Direita
+		p1.SetValues(p1.getLinha(), p1.getColuna() + 1);
+		while(getTab().PositionExists(p1) && canMove(p1)) {
+			mat[p1.getLinha()][p1.getColuna()] = true;
+		}
+		
+		//Noroeste
+		p1.SetValues(p1.getLinha() - 1, p1.getColuna() - 1);
+		while(getTab().PositionExists(p1) && canMove(p1)) {
+			mat[p1.getLinha()][p1.getColuna()] = true;
+		}
+		
+		//Nordeste
+		p1.SetValues(p1.getLinha() - 1, p1.getColuna() + 1);
+		while(getTab().PositionExists(p1) && canMove(p1)) {
+			mat[p1.getLinha()][p1.getColuna()] = true;
+		}
+		
+		//Sudoeste
+		p1.SetValues(p1.getLinha() + 1, p1.getColuna() - 1);
+		while(getTab().PositionExists(p1) && canMove(p1)) {
+			mat[p1.getLinha()][p1.getColuna()] = true;
+		}
+		
+		//Sudeste
+		p1.SetValues(p1.getLinha() + 1, p1.getColuna() + 1);
+		while(getTab().PositionExists(p1) && canMove(p1)) {
+			mat[p1.getLinha()][p1.getColuna()] = true;
+		}
+		
 		return mat;
 	}
 }
